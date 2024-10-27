6b95a9d55d20e8c6c98088537e870407fbd5b8d1
shoppyshops <daryl@shoppyshops.shop>
2024-10-27 18:10:07 +1100

    wip

--- run.py
+++ run.py
@@ -1,20 +1,36 @@
 import aiohttp
-import asyncio 
+import asyncio
+import shopify
+import dotenv
+import os
 
+dotenv.load_dotenv()
 
 class ShoppyShops:
-    def __init__(self):
-        self.session = aiohttp.ClientSession()
+    def __init__(
+        self,
+        shopify_api_secret,
+        shopify_shop_url,
+    ):
+        self.shopify = shopify.Session(
+            private_app_password=shopify_api_secret,
+            api_version="2024-10",
+            shop_url=shopify_shop_url,
+        )
 
-    async def get_shops(self):
-        async with self.session.get("https://shoppy.gg/api/v1/shops") as response:
-            return await response.json()
+    async def get_orders(self):
+        return await self.shopify.get("orders.json")
 
 
 async def run():
-    shops = ShoppyShops()
-    print(await shops.get_shops())
+    local_aussie_store = ShoppyShops(
+        shopify_api_secret=os.getenv("SHOPIFY_API_SECRET"),
+        shopify_shop_url=os.getenv("SHOPIFY_SHOP_URL")
+    )
+
+    print(await local_aussie_store.get_orders())
 
 if __name__ == "__main__":
     asyncio.run(run())
+
 
